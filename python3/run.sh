2b604b6da44efd519e8fa701db01609ca113ccc9
leftjs <lefttjs@gmail.com>
2018-07-14 01:58:42 +0800

    update

--- python3/run.sh
+++ python3/run.sh
@@ -1,2 +1,2 @@
 nvidia-docker rm caffe
-nvidia-docker run -t -i  --ulimit nofile=327680:327680 --ulimit nproc=65536:65536 --name caffe -v /home/jason/DLOS/test:/test -w /test -d splashblot/py-faster-rcnn:latest
+nvidia-docker run -t -i --network="host" --ulimit nofile=327680:327680 --ulimit nproc=65536:65536 --name caffe -v /home/jason/DLOS/test:/test -w /test -d splashblot/py-faster-rcnn:latest
